5401c843299947324ae44393afa9372f7135ba3f
Tryphena P <tryphenap@Tryphenas-MacBook-Pro.local>
2026-07-29 13:27:41 -0700

    Used the useRef hook in TodoForm and event.preventDefault() dynamically add todos in app.jsx

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,11 +1,37 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+function TodoForm({ onAddTodo }) {
+const inputRef = useRef();
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+
+  
+    // .trim prevents whitespace only todos
+    const todoTitle = event.target.todoTitle.value.trim();
+    if (todoTitle && todoTitle !== "") {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus();
+    }
+  };
   return (
-    <form>
-      <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>Add Todo</button>
-    </form>
-  );
+  <form onSubmit={handleAddTodo}>
+    <label htmlFor="todoTitle">Todo</label>
+    <input
+      ref={inputRef}
+      type="text"
+      id="todoTitle"
+      name="todoTitle"
+      placeholder={'Todo text'}
+      required
+    />
+    <button type="submit" >
+      Add Todo
+    </button>
+  </form>
+);
+
 }
 
 export default TodoForm;
